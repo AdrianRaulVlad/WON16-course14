48deaf26cdceddf5f04432d2c2cd3f771e3ecd01
AdrianVlad <adrianraul.vlad@gmail.com>
2024-06-12 23:21:06 +0300

    add the recursive method to calculate the sum of first N and tests for the method

--- src/main/java/org/example/exerciseOne/SumOfFirstN.java
+++ src/main/java/org/example/exerciseOne/SumOfFirstN.java
@@ -9,15 +9,4 @@ public class SumOfFirstN {
             return n + sum(n - 1);
         }
     }
-
-    public static void main(String[] args) {
-        // Create an object of SumOfFirstN
-        SumOfFirstN sumOfFirstN = new SumOfFirstN();
-
-        // Test cases
-        System.out.println("Sum of first 5 integers: " + sumOfFirstN.sum(5)); // Should print 15
-        System.out.println("Sum of first 10 integers: " + sumOfFirstN.sum(10)); // Should print 55
-        System.out.println("Sum of first 1 integer: " + sumOfFirstN.sum(1)); // Should print 1
-        System.out.println("Sum of first 0 integers: " + sumOfFirstN.sum(0)); // Should print 0
-    }
 }
